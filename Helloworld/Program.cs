afece5ac0818c9e9c5640dbad3373b1e09911725
stmdevops <s.thangamuthu82@gmail.com>
2023-12-31 00:58:24 +0530

    Revert "fifth"
    This reverts commit 718f82256329490e5f5a76396d6ea8731b87069a.

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -10,7 +10,7 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to fifth commit");
+            Console.WriteLine("Welcome to fourth commit");
             Console.ReadLine();
         }
     }
